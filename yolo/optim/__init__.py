21b514947928a57eb9e4b2f24e16b7df25d5d4d3
zjkjzj <wy163zhuj@163.com>
2023-04-02 19:43:23 +0800

    style(yolo): remove unused code

--- yolo/optim/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-@date: 2022/12/14 上午10:58
-@file: __init__.py.py
-@author: zj
-@description: 
-"""
--- yolo/util/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-@date: 2023/1/9 下午5:33
-@file: __init__.py.py
-@author: zj
-@description: 
-"""
