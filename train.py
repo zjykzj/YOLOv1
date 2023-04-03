371da57fa8bb27b2bb413b1f49920537d17d8dd4
zjykzj <wy163zhuj@163.com>
2023-04-03 10:49:22 +0800

    perf(train.py): add lr_scheduler and update log

--- train.py
+++ train.py
@@ -14,7 +14,8 @@ from yolo.model.yolov1 import YOLOv1
 from yolo.model.yololoss import YOLOv1Loss
 from yolo.data.vocdataset import VOCDataset
 
-device = torch.device('cpu')
+device = torch.device('cuda:0')
+# device = torch.device('cpu')
 
 # define the YOLOv1 model
 model = YOLOv1().to(device)
@@ -25,7 +26,8 @@ loss_fn = YOLOv1Loss().to(device)
 print(loss_fn)
 
 # define the optimizer
-optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
+optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9, weight_decay=5e-4)
+lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[20, 35, 45], gamma=0.1)
 print(optimizer)
 
 # load the VOC dataset
@@ -37,8 +39,9 @@ train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 
 # train the model
 print("=> Train")
-num_epochs = 20
+num_epochs = 50
 for epoch in range(num_epochs):
+    total_loss = 0.
     for i, (inputs, targets) in enumerate(train_loader):
         # zero the gradients
         optimizer.zero_grad()
@@ -46,11 +49,18 @@ for epoch in range(num_epochs):
         # forward pass
         outputs = model(inputs.to(device))
         loss = loss_fn(outputs, targets.to(device))
+        total_loss += loss.item()
 
         # backward pass
         loss.backward()
         optimizer.step()
 
         # print loss for monitoring training progress
-        # if i % 10 == 0:
-        print(f"Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{len(train_loader)}], Loss: {loss.item():.4f}")
+        if i % 10 == 0:
+            print(
+                f"Epoch [{epoch + 1}/{num_epochs}], "
+                f"Step [{i + 1}/{len(train_loader)}], "
+                f"Lr: {optimizer.state_dict()['param_groups'][0]['lr']:.6f}, "
+                f"Loss: {loss.item():.6f}, "
+                f"Average Loss: {total_loss / (i + 1):.6f}")
+    lr_scheduler.step()
