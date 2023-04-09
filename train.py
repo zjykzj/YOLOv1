d269607c721549524b8d037dd700f206ce944ef7
zjkjzj <wy163zhuj@163.com>
2023-04-09 13:42:21 +0800

    perf(train.py): udate

--- train.py
+++ train.py
@@ -29,12 +29,12 @@ def train(epoch, num_epochs, train_loader, optimizer, model, loss_fn, device):
         # forward pass
         outputs = model(inputs.to(device))
         loss = loss_fn(outputs, targets.to(device))
-        total_loss += loss.item()
 
         # backward pass
         loss.backward()
         optimizer.step()
 
+        total_loss += loss.item()
         # print loss for monitoring training progress
         if i % 10 == 0:
             print(
@@ -44,7 +44,10 @@ def train(epoch, num_epochs, train_loader, optimizer, model, loss_fn, device):
                 f"Loss: {loss.item():.6f}, "
                 f"Average Loss: {total_loss / (i + 1):.6f}")
 
+    return total_loss / len(train_loader)
 
+
+@torch.no_grad()
 def val(epoch, num_epochs, model, device, loss_fn, val_loader):
     model.eval()
 
@@ -53,8 +56,8 @@ def val(epoch, num_epochs, model, device, loss_fn, val_loader):
         # forward pass
         outputs = model(inputs.to(device))
         loss = loss_fn(outputs, targets.to(device))
-        total_loss += loss.item()
 
+        total_loss += loss.item()
         # print loss for monitoring training progress
         if i % 10 == 0:
             print(
@@ -70,12 +73,27 @@ def main():
     device = torch.device('cuda:0')
     # device = torch.device('cpu')
 
+    S = 7
+    B = 2
+    C = 20
+
     # define the YOLOv1 model
-    model = YOLOv1().to(device)
+    # model = YOLOv1().to(device)
+    model = YOLOv1(num_classes=C, S=S, B=B).to(device)
     print(model)
 
+    lambda_coord = 5.
+    lambda_obj = 1.
+    lambda_noobj = 0.5
+    lambda_class = 1.
+    # lambda_coord = 1.
+    # lambda_obj = 1.
+    # lambda_noobj = 1.
+    # lambda_class = 1.
+
     # define the loss function
-    loss_fn = YOLOv1Loss().to(device)
+    loss_fn = YOLOv1Loss(S=S, B=B, C=C, lambda_coord=lambda_coord, lambda_obj=lambda_obj, lambda_noobj=lambda_noobj,
+                         lambda_class=lambda_class).to(device)
     print(loss_fn)
 
     # define the optimizer
@@ -87,31 +105,35 @@ def main():
     # load the VOC dataset
     print("=> Load data")
     root = '/home/zj/yoyo/voc'
-    name = 'yolov1-voc-train'
-    train_dataset = VOCDataset(root, name, train=True, target_size=448)
+    name = 'voc2yolov5-train'
+    train_dataset = VOCDataset(root, name, train=True, B=B, S=S, target_size=448)
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
 
-    name = 'yolov1-voc-val'
-    val_dataset = VOCDataset(root, name, train=False, target_size=448)
+    name = 'voc2yolov5-val'
+    val_dataset = VOCDataset(root, name, train=False, B=B, S=S, target_size=448)
     val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=4)
 
+    best_epoch = 0
     best_loss = np.Inf
 
     # train the model
     num_epochs = 50
     for epoch in range(num_epochs):
         print("=> Train")
-        train(epoch, num_epochs, train_loader, optimizer, model, loss_fn, device)
+        loss = train(epoch, num_epochs, train_loader, optimizer, model, loss_fn, device)
+        print(f"=> Train Loss: {loss:.6f}")
         lr_scheduler.step()
 
         print("=> Val")
         loss = val(epoch, num_epochs, model, device, loss_fn, val_loader)
+        print(f"=> Val Loss: {loss:.6f}")
 
         torch.save(model.state_dict(), 'checkpoint.pth.tar')
         if loss < best_loss:
+            best_epoch = epoch + 1
             best_loss = loss
-            print(f"=> Best loss: {best_loss:.6f}")
             shutil.copy('checkpoint.pth.tar', 'best.pth.tar')
+        print(f"=> Best Epoch: {best_epoch}\tBest loss: {best_loss:.6f}")
 
 
 if __name__ == '__main__':
