12e1988912a4394eb3c2e2ed665200f708fe02cc
zjkjzj <wy163zhuj@163.com>
2023-04-14 10:43:46 +0800

    perf(yololoss.py): update

--- yolo/model/yololoss.py
+++ yolo/model/yololoss.py
@@ -20,14 +20,17 @@ def bbox_iou(boxes1, boxes2):
     N = boxes1.size(0)
     M = boxes2.size(0)
 
+    # [x_min, y_min]
     lt = torch.max(boxes1[:, :2].unsqueeze(1).expand(N, M, 2),
                    boxes2[:, :2].unsqueeze(0).expand(N, M, 2))
+    # [x_max, y_max]
     rb = torch.min(boxes1[:, 2:].unsqueeze(1).expand(N, M, 2),
                    boxes2[:, 2:].unsqueeze(0).expand(N, M, 2))
 
     wh = (rb - lt).clamp(min=0)
     inter = wh[:, :, 0] * wh[:, :, 1]
 
+    # (x_max - x_min) * (y_max - y_min)
     area1 = (boxes1[:, 2] - boxes1[:, 0]) * (boxes1[:, 3] - boxes1[:, 1])
     area2 = (boxes2[:, 2] - boxes2[:, 0]) * (boxes2[:, 3] - boxes2[:, 1])
 
@@ -52,8 +55,6 @@ class YOLOv1Loss(nn.Module):
         # predictions shape: (batch_size, S, S, (B*5+C))
         # targets shape: (batch_size, S, S, (B*5+C))
         assert predictions.shape == targets.shape
-
-        device = predictions.device
 
         N = len(predictions)
         assert self.S == predictions.shape[2] and self.S == predictions.shape[2]
@@ -104,6 +105,7 @@ class YOLOv1Loss(nn.Module):
                         # 计算IoU，IoU最大的设置为响应框，负责预测标注框
                         # [B, 4]: [xc_offset, yc_offset, box_w, box_h]
                         pred_grid_boxes = pred_boxes[ni, i, j]
+                        # [B]
                         pred_xc = (pred_grid_boxes[:, 0] + i) * cell_size
                         pred_yc = (pred_grid_boxes[:, 1] + j) * cell_size
                         pred_w = pred_grid_boxes[:, 2]
@@ -118,8 +120,9 @@ class YOLOv1Loss(nn.Module):
 
                         # [1, 4]
                         target_grid_boxes = target_boxes[ni, i, j, 0].unsqueeze(0)
-                        target_xc = (target_grid_boxes[:, 0] * cell_size + i + 1) * cell_size
-                        target_yc = (target_grid_boxes[:, 1] * cell_size + j + 1) * cell_size
+                        # [1]
+                        target_xc = (target_grid_boxes[:, 0] * cell_size + i) * cell_size
+                        target_yc = (target_grid_boxes[:, 1] * cell_size + j) * cell_size
                         target_w = target_grid_boxes[:, 2]
                         target_h = target_grid_boxes[:, 3]
 
@@ -140,21 +143,25 @@ class YOLOv1Loss(nn.Module):
                         for bi in range(self.B):
                             if bi == max_iou_id:
                                 # 计算响应框置信度损失
-                                loss_for_response += (pred_conf[ni, i, j, max_iou_id] - 1.) ** 2
+                                # ([1] - [1])**2
+                                loss_for_response += (pred_conf[ni, i, j, max_iou_id] - ious[max_iou_id][0]) ** 2
 
                                 # 计算坐标损失
-                                # [4]
-                                loss_for_coord += F.mse_loss(pred_boxes[ni, i, j, max_iou_id],
-                                                             target_boxes[ni, i, j, max_iou_id], reduction='sum')
+                                # [x_offset, y_offset]
+                                loss_for_coord += F.mse_loss(pred_boxes[ni, i, j, max_iou_id][:2],
+                                                             target_boxes[ni, i, j, max_iou_id][:2],
+                                                             reduction='sum')
+                                # [box_w, box_h]
+                                loss_for_coord += F.mse_loss(torch.sqrt(pred_boxes[ni, i, j, max_iou_id][2:]),
+                                                             torch.sqrt(target_boxes[ni, i, j, max_iou_id][2:]),
+                                                             reduction='sum')
                             else:
                                 # 计算不响应框置信度损失
                                 loss_for_noresponse += (pred_conf[ni, i, j, bi] - 0.) ** 2
                     else:
                         # 不包含标注框，仅计算置信度损失
                         # [B]
-                        loss_for_noobj += F.mse_loss(pred_conf[ni, i, j],
-                                                     target_conf[ni, i, j],
-                                                     reduction='sum')
+                        loss_for_noobj += F.mse_loss(pred_conf[ni, i, j], target_conf[ni, i, j], reduction='sum')
 
         loss = self.lambda_coord * loss_for_coord + \
                self.lambda_obj * loss_for_response + \
@@ -165,27 +172,27 @@ class YOLOv1Loss(nn.Module):
 
 
 if __name__ == '__main__':
-    torch.manual_seed(32)
-
-    B = 2
-    S = 7
-    N_cls = 20
-
-    m = YOLOv1Loss(S=S, B=B, C=N_cls)
-    print(m)
-
-    a = torch.randn(1, S, S, 5 * B + N_cls)
-    b = torch.zeros(1, S, S, 5 * B + N_cls)
-    b[:, 2, 3] = torch.abs(torch.randn(5 * B + N_cls))
-    for bi in range(B):
-        b[:, 2, 3, 5 * bi + 4] = 1
-    b[:, 2, 3, 5 * B + 8] = 1
-    loss = m(a, b)
-    print(loss)
-
-    m = m.cuda()
-    loss = m(a.cuda(), b.cuda())
-    print(loss)
+    # torch.manual_seed(32)
+    #
+    # B = 2
+    # S = 7
+    # N_cls = 20
+    #
+    # m = YOLOv1Loss(S=S, B=B, C=N_cls)
+    # print(m)
+    #
+    # a = torch.randn(1, S, S, 5 * B + N_cls)
+    # b = torch.zeros(1, S, S, 5 * B + N_cls)
+    # b[:, 2, 3] = torch.abs(torch.randn(5 * B + N_cls))
+    # for bi in range(B):
+    #     b[:, 2, 3, 5 * bi + 4] = 1
+    # b[:, 2, 3, 5 * B + 8] = 1
+    # loss = m(a, b)
+    # print(loss)
+    #
+    # m = m.cuda()
+    # loss = m(a.cuda(), b.cuda())
+    # print(loss)
 
     # a = torch.abs(torch.randn(3, 4)) * 100
     # # b = torch.abs(torch.randn(4, 4)) * 100
@@ -195,3 +202,15 @@ if __name__ == '__main__':
     # print(c)
     #
     # print(torch.argmax(c, dim=1))
+
+    box1 = torch.abs(torch.randn(10, 4))
+    box2 = torch.abs(torch.randn(23, 4))
+    ious = bbox_iou(box1, box2)
+    print(ious.shape)
+
+    box1 = torch.abs(torch.randn(10, 4))
+    box2 = torch.abs(torch.randn(1, 4))
+    ious = bbox_iou(box1, box2)
+    print(ious.shape)
+
+    print(torch.argmax(ious))
