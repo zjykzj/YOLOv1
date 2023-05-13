8dee5dd1c900ae8f68575ccc538efa880332da0d
zjkjzj <wy163zhuj@163.com>
2023-05-14 06:49:48 +0800

    test(model.py): test fastyolov1

--- test/model.py
+++ test/model.py
@@ -12,11 +12,43 @@ import torch
 from yolo.model.yolov1 import YOLOv1
 
 
-def test_yolov1():
+def test_fastyolov1():
+    print('=> FastYOLOv1')
+
     shape = (1, 3, 448, 448)
     print(f"Input: {shape}")
     data = torch.randn(shape)
-    model = YOLOv1(num_classes=20, S=7, B=2, arch='yolov1', pretrained=None)
+    model = YOLOv1(num_classes=20, S=7, B=2, arch='FastYOLOv1', pretrained=None)
+
+    model.train()
+    outputs = model(data)
+    print(outputs.shape)
+
+    model.eval()
+    outputs = model(data)
+    print(outputs.shape)
+
+    shape = (1, 3, 224, 224)
+    print(f"Input: {shape}")
+    data = torch.randn(shape)
+    model = YOLOv1(S=4)
+
+    model.train()
+    outputs = model(data)
+    print(outputs.shape)
+
+    model.eval()
+    outputs = model(data)
+    print(outputs.shape)
+
+
+def test_yolov1():
+    print('=> YOLOv1')
+
+    shape = (1, 3, 448, 448)
+    print(f"Input: {shape}")
+    data = torch.randn(shape)
+    model = YOLOv1(num_classes=20, S=7, B=2, arch='YOLOv1', pretrained=None)
 
     model.train()
     outputs = model(data)
@@ -42,3 +74,4 @@ def test_yolov1():
 
 if __name__ == '__main__':
     test_yolov1()
+    test_fastyolov1()
